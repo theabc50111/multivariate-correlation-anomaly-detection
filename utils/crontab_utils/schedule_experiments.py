cf36d3a5e7d915cc95d38a8f9c6526a7d47c9541
YWT <theabc50111@gmail.com>
2023-12-23 19:50:05 +0800

    Revise schedule_experiments.py

--- utils/crontab_utils/schedule_experiments.py
+++ utils/crontab_utils/schedule_experiments.py
@@ -4,7 +4,17 @@ from datetime import datetime, timedelta
 from itertools import chain, product, repeat
 from pprint import pprint
 
-data_implement_list = ["--data_implement SP500_20112015_CORR_MEAN_ABOVE_MODERATE_POSITIVE_KEEP", "--data_implement SP500_20112015_CORR_MEAN_BELOW_MODERATE_POSITIVE_KEEP"]  # ["", "--data_implement LINEAR_REG_ONE_CLUSTER_DIM_30_BKPS_0_NOISE_STD_30"]
+data_implement_list = ['--data_implement RAND_1_PCA_CLUSTER_PAIRS_0', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_1',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_2', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_3',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_4', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_5',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_6', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_7',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_8', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_9',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_10', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_11',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_12', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_13',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_14', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_15',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_16', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_17',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_18', '--data_implement RAND_1_PCA_CLUSTER_PAIRS_19',
+ '--data_implement RAND_1_PCA_CLUSTER_PAIRS_20']  # ["", "--data_implement LINEAR_REG_ONE_CLUSTER_DIM_30_BKPS_0_NOISE_STD_30"]
 batch_size_list = [""]
 n_folds_list = [""]  # ["", "--n_folds 2", "--n_folds 3"]
 tr_epochs_list = [""]  # ["", "--tr_epochs 200"]
@@ -53,7 +63,7 @@ if set(map(lambda x: x['gru_l'], args_list)) != {""}:
     model_timedelta_list = [timedelta(hours=1, minutes=0), timedelta(hours=3, minutes=0)]  # The order of elements of model_timedelta_list should comply with the order of elements of args_list
 else:
     num_models = len(args_list)
-    model_timedelta_list = [timedelta(hours=1, minutes=55)]
+    model_timedelta_list = [timedelta(hours=0, minutes=15)]
 
 model_timedelta_list = list(chain.from_iterable(repeat(x, num_models) for x in model_timedelta_list))
 model_timedelta_list = [model_timedelta_list[-1]] + model_timedelta_list  # Use the last element of model_timedelta_list to fill the first element of model_timedelta_list for repeating the whole experiments.
